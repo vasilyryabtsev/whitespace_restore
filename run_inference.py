187b55c06668cdad6c65591c10414ef7df0a4c41
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-09-21 21:48:16 +0300

    refactor + formating

--- run_inference.py
+++ run_inference.py
@@ -1,35 +1,45 @@
 import os
+import logging
+
 from dotenv import load_dotenv
 from src.api import inference, download_weights
 
 
-load_dotenv()
+def main():
+    """Основной скрипт для запуска инференса модели восстановления пробелов."""
+    load_dotenv()
 
-WEIGHTS_URL = os.getenv("WEIGHTS_URL", "dlskflsdkjfl")
-PATH_TO_WEIGHTS = os.getenv("PATH_TO_WEIGHTS", "./best_model.pth")
-PATH_TO_DATA = os.getenv("PATH_TO_DATA")
-assert PATH_TO_DATA is not None, "PATH_TO_DATA must be set in .env"
-PATH_TO_SAVE = os.getenv("PATH_TO_SAVE")
-assert PATH_TO_SAVE is not None, "PATH_TO_SAVE must be set in .env"
-TEXT_COLUMN = os.getenv("TEXT_COLUMN")
-assert TEXT_COLUMN is not None, "TEXT_COLUMN must be set in .env"
-DEVICE = os.getenv("DEVICE")
-assert DEVICE is not None, "DEVICE must be set in .env (e.g., 'cpu' or 'cuda')"
+    weights_url = os.getenv("WEIGHTS_URL", "dlskflsdkjfl")
+    path_to_weights = os.getenv("PATH_TO_WEIGHTS", "./best_model.pth")
+    path_to_data = os.getenv("PATH_TO_DATA")
+    assert path_to_data is not None, "PATH_TO_DATA must be set in .env"
+    path_to_save = os.getenv("PATH_TO_SAVE")
+    assert path_to_save is not None, "PATH_TO_SAVE must be set in .env"
+    text_column = os.getenv("TEXT_COLUMN")
+    assert text_column is not None, "TEXT_COLUMN must be set in .env"
+    device = os.getenv("DEVICE")
+    assert device is not None, "DEVICE must be set in .env (e.g., 'cpu' or 'cuda')"
+    batch_size = int(os.getenv("BATCH_SIZE", 16))
+    num_workers = int(os.getenv("NUM_WORKERS", 1))
+    threshold = float(os.getenv("THRESHOLD", 0.5))
 
-if __name__ == "__main__":
     # Загрузка весов
     logging.info("Downloading model weights...")
-    download_weights(url=WEIGHTS_URL, path=PATH_TO_WEIGHTS)
+    download_weights(url=weights_url, path=path_to_weights)
     # Инференс
     logging.info("Running inference...")
     inference(
-        path_to_data=PATH_TO_DATA,
-        path_to_save=PATH_TO_SAVE,
-        path_to_weights=PATH_TO_WEIGHTS,
-        text_col=TEXT_COLUMN,
-        device=DEVICE,
-        batch_size=16,
-        num_workers=4,
-        threshold=0.5
+        path_to_data=path_to_data,
+        path_to_save=path_to_save,
+        path_to_weights=path_to_weights,
+        text_col=text_column,
+        device=device,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        threshold=threshold,
     )
     logging.info("Inference completed!")
+
+
+if __name__ == "__main__":
+    main()
